d58783e9c8ec746149de6bb587441a5d5c9796ac
schvabodka-man <scvhapps@gmail.com>
2017-05-28 20:25:25 +0300

    links on similar games covers

--- app/src/main/java/apps/scvh/com/thegamesdbclient/frontend/list/SameGamesAdapter.java
+++ app/src/main/java/apps/scvh/com/thegamesdbclient/frontend/list/SameGamesAdapter.java
@@ -2,12 +2,12 @@ package apps.scvh.com.thegamesdbclient.frontend.list;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.squareup.picasso.Picasso;
 
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 
 import apps.scvh.com.thegamesdbclient.R;
 import apps.scvh.com.thegamesdbclient.backend.models.GameData;
+import apps.scvh.com.thegamesdbclient.frontend.activities.Game;
 
 public class SameGamesAdapter extends RecyclerView.Adapter<SameGamesAdapter.ViewHolder> {
 
@@ -37,6 +38,7 @@ public class SameGamesAdapter extends RecyclerView.Adapter<SameGamesAdapter.View
     public void onBindViewHolder(ViewHolder holder, int position) {
         GameData gameData = data.get(position);
         Picasso.with(context).load(gameData.getCoverURL()).resize(200, 200).into(holder.cover);
+        holder.clickInit(gameData.getId(), context);
     }
 
     @Override
@@ -46,13 +48,20 @@ public class SameGamesAdapter extends RecyclerView.Adapter<SameGamesAdapter.View
 
     static class ViewHolder extends RecyclerView.ViewHolder {
 
-        LinearLayout linearLayout;
         ImageView cover;
 
         ViewHolder(View itemView) {
             super(itemView);
-            linearLayout = (LinearLayout) itemView;
             cover = (ImageView) itemView.findViewById(R.id.same_game_cover);
+        }
+
+        void clickInit(int gameId, Context context) {
+            cover.setOnClickListener(v -> {
+                Intent intent = new Intent(context, Game.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra(context.getString(R.string.bundle_id), gameId);
+                context.startActivity(intent);
+            });
         }
     }
 }
